f0faaf0aca59371b72423e194ca8c13fd61c0a4e
MetaFomos <super008008@outlook.com>
2022-12-15 02:18:04 -0600

    finish login page

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,76 +1,134 @@
-import React, {useState} from 'react'
-import { Dispatch } from 'redux'
-import { useDispatch, useSelector } from 'react-redux'
-import { login } from '../../redux/auth/actions'
-import { Navigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { Dispatch } from "redux";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../redux/auth/actions";
+import { Navigate } from "react-router-dom";
 
 interface ISignInProps {}
 
-export const SignIn:React.FC<ISignInProps> = () => {
-    const isAuthenticated = useSelector((state: any) => state.auth.isAuthenticated);
-    const dispatch: Dispatch<any> = useDispatch();
-    const [formData, setFormData] = useState({
-        email: '',
-        password: ''
-    })
-    const [loading, setLoading] = useState(false)
-    const onChange = (e: any) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value })
-    }
-    const onLogin = async () => {
-        setLoading(true)
-        await dispatch(login(formData))
-        setLoading(false)
-    }
-    if (isAuthenticated) {
-        return <Navigate to="/dashboard" />
-    }
-    return (
-        <React.Fragment>
-            <div className="hero pt-20">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left ml-5">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6 text-xl">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input 
-                                    type="text" 
-                                    placeholder="email" 
-                                    className="input input-bordered" 
-                                    name="email" 
-                                    value={formData.email} 
-                                    onChange={onChange}
-                                />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input 
-                                    type="password" 
-                                    placeholder="password" 
-                                    className="input input-bordered" 
-                                    name="password"
-                                    value={formData.password}
-                                    onChange={onChange}
-                                />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className={`btn btn-primary ${loading ? 'loading' : ''}`} onClick={() => onLogin()}>Login</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+export const SignIn: React.FC<ISignInProps> = () => {
+  const isAuthenticated = useSelector(
+    (state: any) => state.auth.isAuthenticated
+  );
+  const dispatch: Dispatch<any> = useDispatch();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const onChange = (e: any) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const onLogin = async (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    await dispatch(login(formData));
+    setLoading(false);
+  };
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
+  return (
+    <>
+      <div className="w-full h-[85vh] flex items-center">
+        <div className="w-full lg:w-4/12 px-4 m-auto">
+          <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
+            <div className="rounded-t mb-0 px-6 py-6">
+              <div className="text-center mb-3">
+                <h6 className="text-gray-600 text-sm font-bold">
+                  Sign in with
+                </h6>
+              </div>
+              <div className="btn-wrapper text-center">
+                <button
+                  className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
+                  type="button"
+                  style={{ transition: "all .15s ease" }}
+                >
+                  <img
+                    alt="..."
+                    className="w-5 mr-1"
+                    src={"/assets/img/github.svg"}
+                  />
+                  Github
+                </button>
+                <button
+                  className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
+                  type="button"
+                  style={{ transition: "all .15s ease" }}
+                >
+                  <img
+                    alt="..."
+                    className="w-5 mr-1"
+                    src={"/assets/img/google.svg"}
+                  />
+                  Google
+                </button>
+              </div>
+              <hr className="mt-6 border-b-1 border-gray-400" />
             </div>
-        </React.Fragment>
-    )
-}
+            <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+              <div className="text-gray-500 text-center mb-3 font-bold">
+                <small>Or sign in with credentials</small>
+              </div>
+              <form>
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                    placeholder="Email"
+                    style={{ transition: "all .15s ease" }}
+                    name="email" 
+                    value={formData.email} 
+                    onChange={onChange}
+                  />
+                </div>
+
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                    placeholder="Password"
+                    style={{ transition: "all .15s ease" }}
+                    name="password"
+                    value={formData.password}
+                    onChange={onChange}
+                  />
+                </div>
+                <div>
+                  <label className="inline-flex items-center cursor-pointer">
+                    <input
+                      id="customCheckLogin"
+                      type="checkbox"
+                      className="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
+                      style={{ transition: "all .15s ease" }}
+                    />
+                    <span className="ml-2 text-sm font-semibold text-gray-700">
+                      Remember me
+                    </span>
+                  </label>
+                </div>
+
+                <div className="text-center mt-6">
+                  <button type="button" className={`btn btn-dark w-full ${loading ? 'loading' : ''}`} onClick={onLogin}>Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
